254d6099f2ab8091b679150b7877208960f6f372
Snehil kumar <snehilkumar4c@gmail.com>
2022-09-17 09:58:20 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var numberOfDrums = document.querySelectorAll(".drum").length;
+
+for(var i = 0; i < numberOfDrums; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        
+        this.style.color = "white";
+
+    });
+}
+
+
+
+
+// var audio = new Audio("sounds/tom-1.mp3");
+//         audio.play();
